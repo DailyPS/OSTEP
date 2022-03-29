9676bccdd3ceb3265802e407c98c74b7327e6675
DailyLinux <baksa1849@gmail.com>
2022-03-29 20:31:49 +0900

    Some Revise.

--- 19/tlb.c
+++ 19/tlb.c
@@ -6,17 +6,14 @@
 int main(int argc, char *argv[])
 {
   long PAGESIZE = sysconf(_SC_PAGESIZE); // 4096
-  long jump = PAGESIZE / sizeof(int);
+  long jump = PAGESIZE / sizeof(int); // 1024
   int NUMPAGES = atoi(argv[1]);
   int trial = atoi(argv[2]);
 
-  printf("%ld", PAGESIZE);
-  printf("\n");
-
-  int *a = calloc(NUMPAGES, PAGESIZE);
+  int *a = calloc(NUMPAGES, PAGESIZE); // Initialize
   struct timespec s, e;
 
-  clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &s);
+  clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &s); // strat check running time
 
   for (int t = 0; t < trial; t++)
   {
@@ -24,7 +21,7 @@ int main(int argc, char *argv[])
       a[i] += 1;
   }
 
-  clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &e);
+  clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &e); // end check running time
 
   printf("%lf\n", ((e.tv_sec - s.tv_sec) * 1e9 + (e.tv_nsec - s.tv_nsec)) / (trial * NUMPAGES));
 
--- 19/tlb.sh
+++ 19/tlb.sh
@@ -4,7 +4,7 @@ t=1
 a=1
 while [ $t -lt 14 ]
 do
-  ./tlb $a 1000
+  ./tlb $a 100000
   t=`expr $t + 1`
   a=`expr $a \* 2`
 done
